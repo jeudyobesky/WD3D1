1e95a1f7ed4505e7263a1044c0168d46c826eaa4
jeudyobesky <jeudy.obesky@gmail.com>
2021-04-20 02:01:54 +0200

    ---

---
+++ lib/02_calculator.rb
@@ -0,0 +1,23 @@
+def add (number1, number2)
+    return number1 + number2
+end
+
+def subtract (number1, number2)
+    return number1 - number2
+end
+
+def sum (array)
+    total = 0
+    array.each do |i|
+        total += i
+    end
+    return total
+end
+
+def multiply (number1, number2)
+    return number1 * number2    
+end
+
+def power (number1, number2)
+    return number1 ** number2    
+end
